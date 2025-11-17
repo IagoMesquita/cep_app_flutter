17e689caf84814b8951e3b072fee9bfc0b5b324e
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-11-17 07:30:53 -0300

    feat: add contract ai service

---
+++ lib/shared/data/remote/api_service.dart
@@ -0,0 +1,7 @@
+import 'package:cep_app/shared/data/async/either.dart';
+import 'package:cep_app/shared/data/models/api_response_model.dart';
+import 'package:cep_app/shared/data/remote/errors/api_exception.dart';
+
+abstract class ApiService {
+  Future<Either<ApiException, ApiResponseModel>> get<T>(String endPoint, {Map<String, dynamic>? queryParams});
+}
