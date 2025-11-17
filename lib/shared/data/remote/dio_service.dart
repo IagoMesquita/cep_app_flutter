9fe94a962bb78f995c24a1b1419800bf07578d68
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-11-17 07:34:24 -0300

    feat: injectble DIO in Dio Service

---
+++ lib/shared/data/remote/dio_service.dart
@@ -0,0 +1,19 @@
+import 'package:cep_app/shared/data/async/either.dart';
+import 'package:cep_app/shared/data/models/api_response_model.dart';
+import 'package:cep_app/shared/data/remote/api_service.dart';
+import 'package:cep_app/shared/data/remote/errors/api_exception.dart';
+import 'package:dio/dio.dart';
+
+final class DioService implements ApiService {
+  final Dio _dio;
+
+  DioService(this._dio);
+
+  @override
+  Future<Either<ApiException, ApiResponseModel>> get<T>(
+    String endPoint, {
+    Map<String, dynamic>? queryParams,
+  }) {
+    try {} catch (e) {}
+  }
+}
