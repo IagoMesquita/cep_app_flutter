46f6298a4325356d9f8050a99ba4c67d47d2eac1
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-11-17 08:08:04 -0300

    feat: add exception SocketException

--- lib/shared/data/remote/dio_service.dart
+++ lib/shared/data/remote/dio_service.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:cep_app/shared/data/async/either.dart';
 import 'package:cep_app/shared/data/models/api_response_model.dart';
 import 'package:cep_app/shared/data/remote/api_service.dart';
@@ -28,6 +31,18 @@ final class DioService implements ApiService {
           statusMessage: statusMessage,
         ),
       );
-    } catch (e) {}
+      // Erro de falha na internet ao fazer uma req http
+    } on SocketException catch (error, st) {
+      const identifier = 'Socket Exception on Get Request';
+      log(identifier, error: error, stackTrace: st);
+
+      return Left(
+        ApiException(
+          identifier: identifier,
+          statusCode: 1, //Status code do front. Documentar para a equipe.
+          errorStatus: ErrorStatus.noConnection,
+        ),
+      );
+    }
   }
 }
