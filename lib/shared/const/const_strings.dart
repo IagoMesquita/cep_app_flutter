26c831da4ce286d0dc140a67db8a401eab8e8304
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-11-17 09:00:37 -0300

    feat: add const strings

---
+++ lib/shared/const/const_strings.dart
@@ -0,0 +1,5 @@
+sealed class ConstStrings {
+  static const String kDefaultError = 'Ocorreu um erro. Por favor, tente novamente mais tarde';
+
+  static const String kNoInternetConnectionMessage = 'Você está sem conexão com a internet. Tente novamente mais tarde';
+}
