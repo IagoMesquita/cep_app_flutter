b7a7c8cf50649b003b22e8c4fefcc41d7b3bb267
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-01-07 08:23:17 -0300

    refactor: change return

--- lib/shared/ui/theme/domain/repositories/theme_repository.dart
+++ lib/shared/ui/theme/domain/repositories/theme_repository.dart
@@ -2,7 +2,7 @@ import 'package:cep_app/shared/data/async/either.dart';
 import 'package:cep_app/shared/ui/theme/errors/theme_local_exception.dart';
 
 abstract class ThemeRepository {
-  Future<bool> getIsLightThe();
+  Future<Either<ThemeLocalException, bool>> getIsLightThe();
 
   Future<Either<ThemeLocalException, void>> setIsLightThemr(bool isLightThe);
 }
