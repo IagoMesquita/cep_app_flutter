cf316f7e7637cfab30ae7737f38214c53b42fd33
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-01-12 11:48:13 -0300

    feat: add provider scope and consume theme for state theme

--- lib/shared/main/cep_app.dart
+++ lib/shared/main/cep_app.dart
@@ -1,6 +1,10 @@
+import 'package:cep_app/features/cep/presentation/screens/cep_screen.dart';
 import 'package:cep_app/shared/main/cep_config.dart';
 import 'package:cep_app/shared/ui/theme/cep_app_theme.dart';
+import 'package:cep_app/shared/ui/theme/domain/providers/theme_notifier_provider.dart';
+import 'package:cep_app/shared/ui/theme/domain/providers/theme_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CepApp extends StatelessWidget {
   const CepApp({super.key});
@@ -8,12 +12,18 @@ class CepApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: CepConfig.title,
-      theme: CepAppTheme.light,
-      darkTheme: CepAppTheme.dark,
-      themeMode: ThemeMode.dark,
-      home: Container(child: Center(child: Text("BUSCA CEP"),),),
+    return ProviderScope(
+      child: Consumer(
+        builder: (_, ref, __) {
+          return MaterialApp(
+            title: CepConfig.title,
+            theme: CepAppTheme.light,
+            darkTheme: CepAppTheme.dark,
+            themeMode: ref.watch<ThemeState>(themeNotifierProvider).getTheme,
+            home: const CepScreen(),
+          );
+        },
+      ),
     );
   }
 }
