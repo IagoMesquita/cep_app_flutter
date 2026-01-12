59331ed121d1b05f3b5ab92dec63da1288caaea5
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-01-12 11:46:33 -0300

    feat: implements theme repository

--- lib/shared/ui/theme/domain/repositories/theme_repository.dart
+++ lib/shared/ui/theme/domain/repositories/theme_repository.dart
@@ -2,7 +2,7 @@ import 'package:cep_app/shared/data/async/either.dart';
 import 'package:cep_app/shared/ui/theme/errors/theme_local_exception.dart';
 
 abstract class ThemeRepository {
-  Future<Either<ThemeLocalException, bool>> getIsLightThe();
+  Future<Either<ThemeLocalException, bool>> getIsLightTheme();
 
-  Future<Either<ThemeLocalException, void>> setIsLightThemr(bool isLightThe);
+  Future<Either<ThemeLocalException, void>> setIsLightTheme(bool isLightThe);
 }
