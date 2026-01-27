32cfc0d6b9ae095480f0c07d8e92db365f1c16b8
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-01-27 08:14:26 -0300

    feat: add exceptioon LocalDetailsInternetConnectionException

--- lib/features/cep/data/data_sources/erros/cep_exceptions.dart
+++ lib/features/cep/data/data_sources/erros/cep_exceptions.dart
@@ -1,3 +1,4 @@
+import 'package:cep_app/features/cep/data/models/cep_response_model.dart';
 import 'package:cep_app/features/cep/domain/entities/cep_response.dart';
 import 'package:cep_app/features/cep/domain/errors/cep_exception.dart';
 import 'package:cep_app/shared/const/const_strings.dart';
@@ -14,5 +15,12 @@ final class CepInterConnectionException extends CepException {
   final CepResponse? cep;
 
   CepInterConnectionException({this.cep})
+    : super(message: ConstStrings.kNoInternetConnectionMessage);
+}
+
+final class LocalDetailsInternetConnectionException extends CepException {
+  final List<CepResponseModel>? cepList;
+
+  LocalDetailsInternetConnectionException({this.cepList})
     : super(message: ConstStrings.kNoInternetConnectionMessage);
 }
