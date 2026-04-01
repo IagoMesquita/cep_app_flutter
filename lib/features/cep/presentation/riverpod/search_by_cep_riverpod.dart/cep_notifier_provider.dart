b720c26b8cd9b5e0d372722e3029c7bf5020eedd
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-04-01 08:53:21 -0300

    fix: add types for providers

--- lib/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/cep_notifier_provider.dart
+++ lib/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/cep_notifier_provider.dart
@@ -1,12 +1,14 @@
 import 'package:cep_app/features/cep/domain/providers/get_cep_details_provider.dart';
 import 'package:cep_app/features/cep/domain/use_cases/get_cep_details_by_cep.dart';
 import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/search_by_cep_notifier.dart';
+import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/search_by_cep_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final searchByCepNotifierProvider = StateNotifierProvider(
-  (ref) {
-    final getCepDetailsByCepInstance = ref.read<GetCepDetailsByCep>(getCepDetailsByCep);
+final searchByCepNotifierProvider =
+    StateNotifierProvider<SearchByCepNotifier, SearchByCepState>((ref) {
+      final getCepDetailsByCepInstance = ref.read<GetCepDetailsByCep>(
+        getCepDetailsByCep,
+      );
 
-    return SearchByCepNotifier(getCepDetailsByCepInstance);
-  }
-);
+      return SearchByCepNotifier(getCepDetailsByCepInstance);
+    });
