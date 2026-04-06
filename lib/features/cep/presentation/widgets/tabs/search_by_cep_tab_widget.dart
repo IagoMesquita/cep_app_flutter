2096dc25509d983895575ec7d94eb5aefdd495c8
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-04-06 09:11:22 -0300

    feat: add CepTextFieldWidget and CepButtonWidget in SearchByCepTabWidget

--- lib/features/cep/presentation/widgets/tabs/search_by_cep_tab_widget.dart
+++ lib/features/cep/presentation/widgets/tabs/search_by_cep_tab_widget.dart
@@ -1,8 +1,12 @@
 import 'package:cep_app/features/cep/domain/entities/get_cep_details_by_cep_body.dart';
 import 'package:cep_app/features/cep/presentation/cep_tec_mixin.dart';
+import 'package:cep_app/features/cep/presentation/constants/validation_messages_const.dart';
+import 'package:cep_app/features/cep/presentation/riverpod/base_cep_app_state.dart';
 import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/cep_notifier_provider.dart';
 import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/search_by_cep_notifier.dart';
 import 'package:cep_app/features/cep/presentation/riverpod/search_by_cep_riverpod.dart/search_by_cep_state.dart';
+import 'package:cep_app/features/cep/presentation/widgets/buttons/cep_button_widget.dart';
+import 'package:cep_app/features/cep/presentation/widgets/inputs/cep_text_field_widget.dart';
 import 'package:cep_app/shared/ui/extensions/theme_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -52,7 +56,28 @@ class _SearchByCepTabWidgetState extends ConsumerState<SearchByCepTabWidget>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Insira um CEP', style: context.getTextTheme.titleMedium)
+            Text('Insira um CEP', style: context.getTextTheme.titleMedium),
+            SizedBox(height: 16),
+            CepTextFieldWidget(
+              textEC: cepTEC,
+              placeholder: 'CEP',
+              validator: (String? cep) {
+                if (cep == null || cep.isEmpty) {
+                  return ValidationMessagesConst.notEmpty('CEP');
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 32),
+            CepButtonWidget(
+              label: 'Procurar',
+              onPressed: state.state == CepStateEnum.loading ? null : () {
+                cepInputFN.unfocus();
+                onSearchCep();
+              },
+            ),
+            const SizedBox(height: 32),
+            
           ],
         ),
       ),
