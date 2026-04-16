ba00a8b7d7e8645e100671d3e813822a2f95e84d
Iago Mesquita <iagomesquitaalves@gmail.com>
2026-04-16 07:11:41 -0300

    feat: add validate length

--- lib/features/cep/presentation/constants/validation_messages_const.dart
+++ lib/features/cep/presentation/constants/validation_messages_const.dart
@@ -1,3 +1,7 @@
 sealed class ValidationMessagesConst {
-  static String notEmpty(String inputLabel) => 'Por favor, insira o $inputLabel';
+  static String notEmpty(String inputLabel) =>
+      'Por favor, insira o $inputLabel';
+
+  static String length(String inputLabel, int length) =>
+      '$inputLabel n~ao deve exceder $length${length > 1 ? 's' : ''}';
 }
